52d258f288866129ec827e743f35c74de872853b
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-08-06 23:47:53 -0400

    NOT DONE

---
+++ custom-sorts.js
@@ -0,0 +1,94 @@
+function ageSort(users) {
+    // Your code here
+    let newArr = [];
+    users.sort((a, b) => a.age - b.age);
+
+    for (let i = 0; i < users.length; i++) {
+        newArr.push(users[i].firstName)
+    }
+    return newArr;
+
+}
+
+function oddEvenSort(arr) {
+    // Your code here
+    let odd = [];
+    let even = [];
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            even.push(arr[i]);
+        } else {
+            odd.push(arr[i])
+        }
+    }
+    odd.sort();
+    even.sort();
+    newArr = [...odd, ...even];
+
+    return newArr;
+
+}
+
+function validAnagrams(s, t) {
+    // Your code here
+}
+
+function reverseBaseSort(arr) {
+    // Your code here
+}
+
+function frequencySort(arr) {
+    // Your code here
+}
+
+// const users = [{
+//         id: 1,
+//         firstName: 'John',
+//         lastName: 'Doe',
+//         age: 30,
+//         occupation: 'Software Engineer',
+//         friends: [2, 3, 4]
+//     },
+//     {
+//         id: 2,
+//         firstName: 'Jane',
+//         lastName: 'Doe',
+//         age: 25,
+//         occupation: 'Data Scientist',
+//         friends: [1, 4]
+//     },
+//     {
+//         id: 3,
+//         firstName: 'Mary',
+//         lastName: 'Smith',
+//         age: 32,
+//         occupation: 'UX Designer',
+//         friends: [2, 4]
+//     },
+//     {
+//         id: 4,
+//         firstName: 'James',
+//         lastName: 'Johnson',
+//         age: 55,
+//         occupation: 'CTO',
+//         friends: [1, 2, 3]
+//     }
+// ];
+
+// console.log(ageSort(users));
+
+
+const arr1 = [5, 4, 7, 2, 9, 8, 1, 6, 3];
+const arr2 = [5, 8, 13, 6, 22, 14, 9];
+
+console.log(oddEvenSort(arr1)); // => [1, 3, 5, 7, 9, 2, 4, 6, 8]
+console.log(oddEvenSort(arr2)); // => [5, 9, 13, 6, 8, 14, 22]
+
+module.exports = [
+    oddEvenSort,
+    validAnagrams,
+    reverseBaseSort,
+    frequencySort,
+    ageSort,
+];
