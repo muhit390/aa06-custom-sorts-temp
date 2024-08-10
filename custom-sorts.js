81f627601daddf6f7015c796f43db870387ae56b
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-08-09 22:17:11 -0400

    DONE

--- custom-sorts.js
+++ custom-sorts.js
@@ -29,6 +29,21 @@ function oddEvenSort(arr) {
 
 function validAnagrams(s, t) {
     // Your code here
+
+    if (s.length !== t.length) return false;
+
+    let count = {};
+
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
+
+    return true;
 }
 
 function reverseBaseSort(arr) {
